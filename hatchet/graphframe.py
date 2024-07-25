b1461b58d5129acf0affad37d13d9de81e3cf2d1
Spencer Greene <ssgreene@wpi.edu>
2024-07-25 19:21:12 -0400

    Cleanup/Prep for Object Reader
    This commit follows remarks that #144 will supersede #101, and
    specifically prepares for the integration of both an array reader for
    PerfFlowAspect Array Trace Files (already present) and PerfFlowAspect
    Object Trace Files (to be added). Additionally removes .json files in
    the pytest data directory as no longer needed.

--- hatchet/graphframe.py
+++ hatchet/graphframe.py
@@ -380,11 +380,11 @@ class GraphFrame:
         return HDF5Reader(filename).read(**kwargs)
 
     @staticmethod
-    def from_perfflowaspect(filename, scan_memory=False, scan_cpu=False):
+    def from_perfflowaspect_array(filename, scan_memory=False, scan_cpu=False):
         # import this lazily to avoid circular dependencies
-        from .readers.perfflowaspect_reader import PerfFlowAspectReader
+        from .readers.perfflowaspect_array_reader import PerfFlowAspectArrayReader
 
-        return PerfFlowAspectReader(filename, scan_memory, scan_cpu).read()
+        return PerfFlowAspectArrayReader(filename, scan_memory, scan_cpu).read()
 
     def to_hdf(self, filename, key="hatchet_graphframe", **kwargs):
         # import this lazily to avoid circular dependencies
